cd25aada57400f03cd702e7594ef36287a9b161e
Anmol-Zahra <anmol@fast-shop.co.uk>
2023-04-12 13:02:34 +0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BugApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace BugApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void btnsubmit_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("", "bug button 4 clicked", "OK");
+
+    }
+
+    private void btnclear_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("", "bug button 1 clicked", "OK");
+
+    }
+
+    private void btnpreviousorder_Clicked(object sender, EventArgs e)
+    {
+       
+                DisplayAlert("", "bug button 2 clicked", "OK");
+         
+    }
+
+    private void btnpendingorder_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("", "bug button 3 clicked", "OK");
+
+    }
+}
+
